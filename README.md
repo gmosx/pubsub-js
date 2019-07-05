27d2c4fc7c67ef04726410f3f6eda2473139eea9
George Moschovitis <gmosx@reizu.com>
2019-07-05 21:17:42 +0300

    create repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Broker
+
+A message broker for Web applications.
+
+## FAQ
+
+* Why doesn't `broker` use Observables, Subjects, Subscriptions, etc (e.g. RxJS) ?
---
+++ src/broker.ts
@@ -0,0 +1,35 @@
+export interface Message {
+    type: string
+}
+
+// TODO: introduce a more sophisticated Broker with subject matching.
+
+export type Subscriber = (message: Message) => void
+
+/** Broker mediates messages between publishers and subscribers. */
+export class Broker {
+    private subscribersByMessageType = new Map<string, Subscriber[]>()
+
+    public subscribe(messageType: string, subscriber: Subscriber) {
+        const subscribers = this.subscribersByMessageType.get(messageType) || []
+        subscribers.push(subscriber)
+        this.subscribersByMessageType.set(messageType, subscribers)
+    }
+
+    public unsubscribe(messageType: string, subscriber: Subscriber) {
+        const subscribers = this.subscribersByMessageType.get(messageType) || []
+        const index = subscribers.indexOf(subscriber)
+        if (index > 0) subscribers.splice(index, 1)
+        this.subscribersByMessageType.set(messageType, subscribers)
+    }
+
+    public next(message: Message) { // TODO: Consider publish.
+        const subscribers = this.subscribersByMessageType.get(message.type)
+        if (!subscribers) {
+            return
+        }
+        for (const subscriber of subscribers) {
+            subscriber(message)
+        }
+    }
+}
